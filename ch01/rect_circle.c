151e58c1ba7abf012416bb497d77a8c51fc6c691
Ganesh4 <ganesh@webcontentor.com>
2021-01-21 11:06:03 +0530

    Area Calculation

---
+++ ch01/rect_circle.c
@@ -0,0 +1,46 @@
+/****************************************
+ * The length & breadth of a rectangle and radius of a circle are input through the keyboard.
+ *  Write a program to calculate the area & perimeter of the rectangle,
+ *  and the area & circumference of the circle.
+ * 
+ * 
+ * Input rectangle l b 
+ * 
+ * *** Area - l * b;
+ * *** 2*(l + b)
+ * 
+ * Input circle r
+ * ***** Perimeter  - 2 * pi * r;
+ * ***** Area - pi * r * r;
+ * ******************************************/
+#include<stdio.h>
+int main(){
+    // Assuming the inputs are in int
+    int length, breadth, radius;
+    int area_of_rectangle, perimeter_of_rectangle;
+    float area_of_circle, perimeter_of_circle;
+
+    printf("Enter length of Rectangle:");
+    scanf("%d\n", &length);
+    printf("Enter breadth of Rectangle:");
+    scanf("%d\n", &breadth);
+    printf("Enter Radius of Circle:");
+    scanf("%d\n",&radius);
+
+    // Calculations for Rectangle
+    area_of_rectangle = length * breadth;
+    perimeter_of_rectangle = 2 * (length + breadth);
+
+    // Calculations for Circle
+    area_of_circle = 3.14 * radius * radius;
+    perimeter_of_circle = 2 * 3.14 * radius;
+
+
+    // Display
+    printf("Area of Rectangle %d \n", area_of_rectangle);
+    printf("Perimeter of Rectangle %d \n", perimeter_of_rectangle);
+    printf("Area of Circle %f \n", area_of_circle);
+    printf("Perimeter of Circle %f \n", perimeter_of_circle);
+    
+    return 0;
+}
