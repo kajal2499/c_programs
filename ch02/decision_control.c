dbc222f8ccaf47ad36d723d5044f66235d26adcb
Ganesh4 <ganesh@webcontentor.com>
2021-01-22 10:13:37 +0530

    Decisoon Control excercise

---
+++ ch02/decision_control.c
@@ -0,0 +1,106 @@
+/****
+ * 
+ * if it rains then we are going to the park else we will stay in home
+ * 
+ * if age is greater than 18 then I am elegible for Voting 
+ * if age is greater than 18 then I am elegible for Voting else I am a minor
+ * 
+ * 
+ * *************************/
+#include <stdio.h>
+int main(){
+    // int age = 17;
+    // if(age > 18){ 
+    //    printf("I am eligible for Voting");
+    // } else {
+    //      printf("I am a minor");
+    // }
+
+    // Compare operators
+    // int a,b,c;
+    // a = b = 10;
+    // c = 9;
+    // if(!(a >= b)){
+    //     printf("true");
+    // } else {
+    //     printf("false");
+    // }
+
+    // Expressions in condition
+    // if(2 + 3){ // 5 = and it is true
+    //     printf("I am in if");
+    // } else {
+    //     printf("I am in else");
+    // } // o/p I am in if
+
+    // if(2 - 3){ // -1 = and it is true
+    //     printf("I am in if");
+    // } else {
+    //     printf("I am in else");
+    // } // o/p I am in if
+
+    // if(2 - 2){ // 0 = and it is false
+    //     printf("I am in if");
+    // } else {
+    //     printf("I am in else");
+    // } // o/p I am in else
+
+
+    // variations
+    // only if
+    // if(condition){
+
+    // }
+
+    // // if and else
+    // if(condition){
+
+    // } else {
+
+    // }
+
+    // Nested if else
+    // persons age is greater than 50 and less than 60 and salary is 25000 then print yes else if salary is 50000 print Awesome else print Blahhhh
+    // if(age > 50){ 
+    //     if(age < 60){
+    //         if(salery == 25000){
+    //             print("Yes")
+    //         } else{
+    //             if(salary == 50000){
+    //                 print("Awesomr");
+    //             }else{
+    //                 print("Blahhh")
+    //             }
+    //         }
+    //     }
+
+    // }
+
+
+    // Logical Operators
+
+    // int a = 10;
+    // int b = 0;
+    // int c = 30
+
+    // if(a && b){
+    //     // 10 || 20
+    //     // 0 || 20
+
+    //     // 10 && 20
+    //     // 10 && 0
+    // } else{
+
+    // }
+
+    // if user has a salary greater than 25000 and he is married print Good else print bad
+    
+
+    // if((salary > 25000) && "he is married"){
+    //     // Good
+    // } else{
+    //     // BAd
+    // }
+
+    return 0;
+}
