ece5686fd85a113562a8318fb94f4c540f90a19a
kajal199997 <kajalgayakwad99@gmail.com>
2021-09-05 23:15:50 +0530

    ASCII value of character

---
+++ practice-test/q2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+    char c;
+    printf("\n enter a character");
+    scanf("%c",&c);
+    //%c is displays the character
+    //%d is displays the integer value of a character
+
+    printf("ASCII value of %d",c,c);
+    return 0;
+}
