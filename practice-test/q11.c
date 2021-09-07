ba4183f9ea4c682944618fb963c7da8487bd6798
kajal199997 <kajalgayakwad99@gmail.com>
2021-09-07 16:52:55 +0530

    armstrong or not

--- practice-test/q11.c
+++ practice-test/q11.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-int main() {
+int main()
+ {
   long long n;
   int count = 0;
   printf("Enter an integer: ");
   scanf("%lld", &n);
  
-  // iterate at least once, then until n becomes 0
-  // remove last digit from n in each iteration
-  // increase count by 1 in each iteration
   do {
     n /= 10;
     ++count;
