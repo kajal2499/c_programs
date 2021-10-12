d4a43b79d7c803adecaf64fda9d5d46951611693
kajal199997 <kajalgayakwad99@gmail.com>
2021-10-12 15:44:35 +0530

    reverse triangli

---
+++ practice-test/patern_ques/2quest.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,rows;
+    //printf("input the no of rows"); 
+    //scanf("%d",&rows);
+    for (i= 7;i>=1;i--)
+    {
+        for(j=1;j<=i;j++){
+            printf("%d",j);}
+            printf("\n");
+    }  
+    
+}
